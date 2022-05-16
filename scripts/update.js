2356042fdb2f8fed83b96dc10235a7b4933c3978
theLMGN <me@foxt.dev>
2022-05-16 16:42:59 +0100

    add update.js

---
+++ scripts/update.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+
+var themes = [];
+
+for (var file of fs.readdirSync("./submissions")) {
+    try {
+        var f = JSON.parse(fs.readFileSync("./submissions/" + file,"utf8"))
+        themes.push({
+            name: f.name, author: f.author, tags: f.tags, desc: f.desc,
+            text: f.vars.text, background: f.vars.background, accent: f.vars.accent
+        })
+    } catch(e) {
+        console.error("Error reading " + file + e.toString());
+    }
+}
+
+
+fs.writeFileSync("themes.json", JSON.stringify(themes));
